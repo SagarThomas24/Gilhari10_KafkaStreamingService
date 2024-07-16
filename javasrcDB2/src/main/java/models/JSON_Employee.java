777167a19ca345f3ffe3827f66dba57af9a7c487
SagarThomas24 <156944561+SagarThomas24@users.noreply.github.com>
2024-07-16 19:38:19 +0530

    Add files via upload

---
+++ javasrcDB2/src/main/java/models/JSON_Employee.java
@@ -0,0 +1,15 @@
+package javasrc.src.main.java.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.softwaretree.jdx.JDX_JSONObject;
+
+public class JSON_Employee extends JDX_JSONObject {
+    public JSON_Employee() {         
+        super();     
+    }      
+    public JSON_Employee(JSONObject jsonObject) throws JSONException {         
+        super(jsonObject);     
+    } 
+} 
